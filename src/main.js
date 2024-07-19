6ab76e434332cbb847d95bd31e73163c472da9d1
Makson <144148309+Sky65678@users.noreply.github.com>
2024-07-20 01:14:54 +0300

    main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,59 @@
-import { getPictures } from './pixabay-api';
-import { renderGallery } from './render-functions';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+import { getPictures } from './js/pixabay-api';
+import { renderGallery } from './js/render-functions';
+
 const formEl = document.querySelector('.search-form');
 const inputEl = document.querySelector('.searchInput');
+const loading = document.querySelector('.loading');
 export const gallery = document.querySelector('.gallery');
 
+// Обробник події submit для форми
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const userImg = inputEl.elements.query.value.toLowerCase().trim();
+
+  if (userImg === '') {
+    iziToast.error({
+      position: 'topRight',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+    });
+    return;
+  }
+
+  loading.style.display = 'flex';
+  gallery.innerHTML = '';
+
+  getPictures(userImg)
+    .then(({ hits }) => {
+      if (hits.length === 0) {
+        iziToast.error({
+          title: '',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+          maxWidth: '432px',
+          color: '#ef4040',
+        });
+        gallery.innerHTML = '';
+        loading.style.display = 'none';
+        formEl.reset();
+        return;
+      }
+      renderGallery(hits);
+      loading.style.display = 'none';
+      formEl.reset();
+    })
+    .catch(err => {
+      console.log(err);
+      iziToast.error({
+        title: 'Error',
+        message:
+          'An error occurred while fetching pictures. Please try again later.',
+        position: 'topRight',
+      });
+      loading.style.display = 'none';
+    });
+});
